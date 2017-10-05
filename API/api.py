36d5a73ca630e44723bc5f268f578123606811ab
Alexander Camuto <ac4312@ic.ac.uk>
2017-10-05 12:48:43 +0100

    added LTC and ETH support

--- API/api.py
+++ API/api.py
@@ -7,30 +7,30 @@ from json import dumps
 #Create a engine for connecting to SQLite3.
 #Assuming bitfinexBTCUSD.db is in your app root folder
 
-e = create_engine('sqlite:///bitfinexBTCUSD.db')
+e = create_engine('sqlite:///bitfinex.db')
 
 app = Flask(__name__)
 api = Api(app)
 
 class All_Data(Resource):
-    def get(self):
+    def get(self, coin):
         #Connect to databse
         conn = e.connect()
         #Perform query and return JSON data
-        query = conn.execute("select * from bitfinexBTCUSD")
+        query = conn.execute("select * from %s"%coin)
         return {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 
 class Data_Intervals(Resource):
-    def get(self, date_from, date_to):
+    def get(self, coin, date_from, date_to):
         conn = e.connect()
-        query = conn.execute("select * from bitfinexBTCUSD where Date between '%s' and '%s'"%(date_from,date_to))
+        query = conn.execute("select * from %s where Date between '%s' and '%s'"%(coin, date_from, date_to))
         #Query the result and get cursor.Dumping that data to a JSON is looked by extension
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return result
         #We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
 
-api.add_resource(Data_Intervals, '/data/<string:date_from>/<string:date_to>')
-api.add_resource(All_Data, '/all')
+api.add_resource(Data_Intervals, '/<string:coin>/<string:date_from>/<string:date_to>')
+api.add_resource(All_Data, '/<string:coin>')
 
 
 if __name__ == '__main__':
