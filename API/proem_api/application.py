3ab41dd92e8fd894cd083b0351b537121c00c8d8
Alexander Camuto <ac4312@ic.ac.uk>
2017-10-10 19:21:06 +0100

    compatible with RDS and beanstalk

--- API/proem_api/application.py
+++ API/proem_api/application.py
@@ -8,44 +8,52 @@ from json import dumps
 from datetime import datetime
 from ast import literal_eval
 import requests
+import psycopg2
+import config
 
 application = Flask(__name__)
 # application.config["DEBUG"] = True
 api = Api(application)
 
 supported_currencies = ["BTC","ETH", "LTC"]
-
-e = create_engine('sqlite:///bitfinex.db')
-
+global_keys = ["Date","High","Low","Mid","Last","Bid","Ask","Volume","Coin"]
+def connect_to_database(config) :
+    try:
+        conn=psycopg2.connect(dbname= 'proemdbdev', host= config.database_host,
+        port= '5432', user= 'proem_admin', password= config.RDS_password)
+    except ValueError as valerr:
+        print("Unable to connect to database: " + valerr)
+    return conn
 
 class All_Data(Resource):
     def get(self, coin):
         #Connect to database
-        conn = e.connect()
+        conn = connect_to_database(config)
         #Perform query and return JSON data
         try:
-            query = conn.execute("select * from %s order by date asc"%coin)
+            cursor = conn.cursor()
+            query = cursor.execute("select * from %s order by date asc"%coin)
         except ValueError as valerr:
             print("Failed to extract all data: " + str(valerr))
         #Query the result and get cursor.Dumping that data to a JSON is looked by extension
-        resp = Response(dumps([dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]))
+        keys = [desc[0] for desc in cursor.description]
+        resp = Response(dumps([dict(zip(tuple(keys),i)) for i in cursor]))
+        conn.close()
         #add access control to API.
         resp.headers['Access-Control-Allow-Origin'] = '*'
         return resp
 
 class Current_Data(Resource):
     def get(self, coin):
-        conn = e.connect()
         bfx_coin = 't{0}USD'.format(coin)
         #May seem wasteful but will keep the same names across applications
         try:
-            query = conn.execute("select * from %s"%coin)
             r = literal_eval(requests.get("https://api.bitfinex.com/v2/ticker/%s"%bfx_coin).content)
         except ValueError as valerr:
             print("Failed to get current data from bitfinex: " + str(valerr))
         data = [str(datetime.now())] + [float(r[i]) for i in [-2,-1,-4,0,2,-3]] + [coin]
         data.insert(3,(data[1]+data[2])/2.0)
-        resp = Response(dumps([dict(zip(tuple (query.keys()) ,data))]))
+        resp = Response(dumps([dict(zip(tuple(global_keys) ,data))]))
         resp.headers['Access-Control-Allow-Origin'] = '*'
         return resp
 
@@ -66,12 +74,15 @@ class Data_Candles(Resource):
 
 class Data_Intervals(Resource):
     def get(self, coin, date_from, date_to):
-        conn = e.connect()
+        conn = connect_to_database(config)
         try:
-            query = conn.execute("select * from %s where Date between '%s' and '%s' order by date asc"%(coin, date_from, date_to))
+            cursor = conn.cursor()
+            query = cursor.execute("select * from %s where Date between '%s' and '%s' order by date asc"%(coin, date_from, date_to))
         except ValueError as valerr:
             print("Failed to extract historical data in intervals: " + str(valerr))
-        resp = Response(dumps([dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]))
+        keys = [desc[0] for desc in cursor.description]
+        resp = Response(dumps([dict(zip(tuple(keys) ,i)) for i in cursor]))
+        conn.close()
         resp.headers['Access-Control-Allow-Origin'] = '*'
         return resp
         #We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
