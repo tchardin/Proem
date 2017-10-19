bdef433dcdb8be875b5da912ea22ce937866402e
Alexander Camuto <ac4312@ic.ac.uk>
2017-10-19 10:13:20 +0100

    repair directories

--- API/maintain_database.sh
+++ API/maintain_database.sh
@@ -1,6 +1,6 @@
-# cd /home/ec2-user/Proem/API/data_csv
-cd data_csv
+cd /home/ec2-user/Proem/API/data_csv
+# cd data_csv
 ./new_csv.sh
-# cd /home/ec2-user/Proem/API/
-cd -
+cd /home/ec2-user/Proem/API/
+# cd -
 python maintain_db.py
