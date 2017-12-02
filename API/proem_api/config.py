a8a646ecfffca4431f32691f94ac6079ff23065d
Alexander Camuto <ac4312@ic.ac.uk>
2017-12-02 01:22:30 +0000

    Cleaned
    Cleaned

--- API/proem_api/config.py
+++
@@ -1,7 +0,0 @@
-
-#database_host = 'proemdb-dev.c6mmcimaihhy.us-west-2.rds.amazonaws.com'
-database_host = 'proemdbdev.czaefluoxrgv.us-east-1.rds.amazonaws.com'
-
-RDS_password = 'iloveproems'
-# Uncomment the line below if you want to work with a local DB
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
